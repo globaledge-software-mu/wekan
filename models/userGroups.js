db8cf126f3d95a21e26f6d1f8ef64a34ffd405b6
Tubbrez <tubbrez@globaledge-software.com>
2019-12-20 17:05:43 +0400

    Created a model named 'UserGroups'

---
+++ models/userGroups.js
@@ -0,0 +1,69 @@
+UserGroups = new Mongo.Collection('user_groups');
+
+UserGroups.attachSchema(new SimpleSchema({
+  title: {
+    type: String,
+  },
+  action: {
+    type: String,
+  },
+  quota: {
+    type: Number,
+  },
+  resource: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    autoValue() { // eslint-disable-line consistent-return
+      if (this.isInsert) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    },
+  },
+  modifiedAt: {
+    type: Date,
+    optional: true,
+    autoValue() { // eslint-disable-line consistent-return
+      if (this.isUpdate) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    },
+  },
+}));
+
+UserGroups.helpers({
+	//
+});
+
+UserGroups.allow({
+  insert() {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  update() {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  remove(userId, doc) {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  fetch: [],
+})
