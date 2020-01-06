0faf0be0ca1dcdd899ae6e74b374a4349debf603
Tubbrez <tubbrez@globaledge-software.com>
2020-01-06 11:00:14 +0400

    Create model 'AssignedUserGroups' and collection 'assigned_user_groups'. Fields: userId, userGroupId, groupOrder & quota_used

---
+++ models/assignedUserGroups.js
@@ -0,0 +1,71 @@
+AssignedUserGroups = new Mongo.Collection('assigned_user_groups');
+
+AssignedUserGroups.attachSchema(new SimpleSchema({
+	userId: {
+    type: String,
+  },
+  userGroupId: {
+    type: String,
+  },
+  groupOrder: {
+    type: String,
+  },
+  quota_used: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    autoValue() { // eslint-disable-line consistent-return
+      if (this.isInsert) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    },
+  },
+  modifiedAt: {
+    type: Date,
+    optional: true,
+    autoValue() { // eslint-disable-line consistent-return
+      if (this.isUpdate) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    },
+  },
+}));
+
+AssignedUserGroups.helpers({
+	//
+});
+
+AssignedUserGroups.allow({
+  insert() {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  update() {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  remove(userId, doc) {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  fetch: [],
+})
+
+
