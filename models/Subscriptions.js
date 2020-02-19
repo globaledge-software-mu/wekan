9d9fc39b8bfcc7161eb0fe0b6d7942052642e61a
Tubbrez <tubbrez@globaledge-software.com>
2020-02-19 11:38:08 +0400

    Created Model 'Subscription' with the collection schema bearing its properties: planId, userGroupId, subscribedOn, expiresOn, billingCycle (yearly/monthly), priceSubscribedTo (currently use the price directly from the plan's document)

---
+++ models/Subscriptions.js
@@ -0,0 +1,65 @@
+Subscriptions = new Mongo.Collection('subscriptions');
+
+Subscriptions.attachSchema(new SimpleSchema({
+	planId: {
+    type: String,
+  },
+  userGroupId: {
+    type: String,
+  },
+  subscribedOn: {
+    type: Date,
+  },
+  expiresOn: {
+    type: Date,
+  },
+  billingCycle: { // yearly/monthly
+    type: String,
+  },
+  priceSubscribedTo: { // currently use the price directly from the plan's document
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    autoValue() { // eslint-disable-line consistent-return
+      if (this.isInsert) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    },
+  },
+  modifiedAt: {
+    type: Date,
+    optional: true,
+    autoValue() { // eslint-disable-line consistent-return
+      if (this.isUpdate) {
+        return new Date();
+      } else {
+        this.unset();
+      }
+    },
+  },
+}));
+
+Subscriptions.helpers({
+	//
+});
+
+Subscriptions.allow({
+  insert() {
+  	return true;
+  },
+  update() {
+  	return true;
+  },
+  remove(userId, doc) {
+    const user = Users.findOne(Meteor.user()._id);
+    if (user && user.isAdmin) {
+    	return true;
+    } else {
+    	return false;
+    }
+  },
+  fetch: [],
+})
